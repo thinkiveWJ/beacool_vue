8afd84e7d46d1c8c0fe540b513c03ee35e2da9b1
thinkiveWJ <wangjunzmd@163.com>
2017-10-20 16:40:07 +0800

    ie兼容问题

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import 'classlist-polyfill'
 import 'babel-polyfill'
+import 'eventsource-polyfill'
 import App from './App'
 import router from './router'
 import VueRouter from 'vue-router'
